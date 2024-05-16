2d1ba3eaf524af9247bd0a7051e06fb5af8bd9c8
Răzvan Sbîngu <razsbng@gmail.com>
2024-05-16 03:07:52 +0300

    landing-page v1.0.0

--- apps/landing-page/tailwind.config.mjs
+++ apps/landing-page/tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Quicksand", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-};
+}
