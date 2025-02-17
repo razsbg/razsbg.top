37f7b55a93418b400d8498dc1d22461a2c135e15
Răzvan Sbîngu <razsbng@gmail.com>
2025-02-17 15:39:16 +0200

    typography - add line-height, remove colors

--- apps/landing-page/src/styles/typography.ts
+++ apps/landing-page/src/styles/typography.ts
@@ -1,11 +1,10 @@
 const typography = {
-  "heading-1": "text-4xl xl:text-[44px] font-semibold",
-  "heading-2":
-    "text-2xl sm:text-3xl xl:text-4xl font-semibold uppercase text-teal-500",
+  "heading-1": "text-4xl xl:text-[44px] font-semibold leading-snug",
+  "heading-2": "text-2xl sm:text-3xl xl:text-4xl font-semibold uppercase",
   subtitle: "text-xl sm:text-2xl xl:text-3xl font-medium",
   "top-ten-list": "text-lg sm:text-xl lg:text-2xl xl:text-3xl",
-  footer: "text-sm sm:text-base xl:text-xl text-zinc-50",
-  copyright: "text-xs sm:text-sm xl:text-lg font-medium text-zinc-400",
+  footer: "text-sm sm:text-base xl:text-xl",
+  copyright: "text-xs sm:text-sm xl:text-lg font-medium",
 } as const
 
 export default typography
