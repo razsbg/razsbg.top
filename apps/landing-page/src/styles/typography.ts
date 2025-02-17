7a4ab3a324a9a002999f3ff1ca586f80d1f3abb1
Răzvan Sbîngu <razsbng@gmail.com>
2025-02-17 15:39:16 +0200

    typography - add line-height to heading-1

--- apps/landing-page/src/styles/typography.ts
+++ apps/landing-page/src/styles/typography.ts
@@ -1,5 +1,5 @@
 const typography = {
-  "heading-1": "text-4xl xl:text-[44px] font-semibold",
+  "heading-1": "text-4xl xl:text-[44px] font-semibold leading-snug",
   "heading-2":
     "text-2xl sm:text-3xl xl:text-4xl font-semibold uppercase text-teal-500",
   subtitle: "text-xl sm:text-2xl xl:text-3xl font-medium",
