93e3c47440d0cea4c604976276357c3ad274974f
Răzvan Sbîngu <razsbng@gmail.com>
2025-02-19 13:06:40 +0200

    typography - remove uppercase

--- apps/landing-page/src/styles/typography.ts
+++ apps/landing-page/src/styles/typography.ts
@@ -1,6 +1,6 @@
 const typography = {
   "heading-1": "text-4xl xl:text-[44px] font-semibold leading-snug",
-  "heading-2": "text-2xl sm:text-3xl xl:text-4xl font-semibold uppercase",
+  "heading-2": "text-2xl sm:text-3xl xl:text-4xl font-semibold",
   subtitle: "text-xl sm:text-2xl xl:text-3xl font-medium",
   "top-ten-list": "text-lg sm:text-xl lg:text-2xl xl:text-3xl",
   footer: "text-sm sm:text-base xl:text-xl",
