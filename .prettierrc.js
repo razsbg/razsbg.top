0afd94308367f300cdeda9afe042c8e30c433e60
Răzvan Sbîngu <razsbng@gmail.com>
2025-10-27 17:48:57 +0200

    test driving factory.ai CLI

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: { parser: "astro" },
+    },
+  ],
+  tabWidth: 2,
+  semi: false,
+  printWidth: 80,
+  arrowParens: "avoid",
+}
