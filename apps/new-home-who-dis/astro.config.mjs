f5fa0e0f4eeb11f104aea1db0ad2dcce8fc96903
Răzvan Sbîngu <razsbng@gmail.com>
2025-11-06 23:27:39 +0200

    i don't even know what i'm doing anymore, ep02

--- apps/new-home-who-dis/astro.config.mjs
+++ apps/new-home-who-dis/astro.config.mjs
@@ -1,29 +1,29 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import solid from "@astrojs/solid-js"
-import vercel from "@astrojs/vercel/serverless"
-import node from "@astrojs/node"
+// import vercel from "@astrojs/vercel/serverless"
+// import node from "@astrojs/node"
 
 export default defineConfig({
   integrations: [tailwind(), solid()],
-  output: "server",
-  adapter:
-    process.env.NODE_ENV === "production"
-      ? vercel()
-      : node({
-          mode: "standalone",
-        }),
-  server: {
-    port: 4000,
-    host: true,
-  },
+  // output: "server",
+  // adapter:
+  //   process.env.NODE_ENV === "production"
+  //     ? vercel()
+  //     : node({
+  //         mode: "standalone",
+  //       }),
+  // server: {
+  //   port: 4000,
+  //   host: true,
+  // },
   // vite: {
   //   define: {
   //     __REVOLUT_USERNAME__: JSON.stringify("razsbg"),
   //   },
   // },
-  site:
-    process.env.NODE_ENV === "production"
-      ? "https://party.razsbg.top"
-      : "http://localhost:4000",
+  // site:
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://party.razsbg.top"
+  //     : "http://localhost:4000",
 })
