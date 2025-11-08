d29e96735a957913e3c125c22285e4140b24d0dd
Răzvan Sbîngu <razsbng@gmail.com>
2025-11-08 07:49:57 +0200

    fix astro config, remove vercel config & packages, update 'start' command

--- apps/new-home-who-dis/astro.config.mjs
+++ apps/new-home-who-dis/astro.config.mjs
@@ -1,29 +1,18 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import solid from "@astrojs/solid-js"
-// import vercel from "@astrojs/vercel/serverless"
-// import node from "@astrojs/node"
+import node from "@astrojs/node"
 
 export default defineConfig({
   integrations: [tailwind(), solid()],
-  // output: "server",
-  // adapter:
-  //   process.env.NODE_ENV === "production"
-  //     ? vercel()
-  //     : node({
-  //         mode: "standalone",
-  //       }),
-  // server: {
-  //   port: 4000,
-  //   host: true,
-  // },
-  // vite: {
-  //   define: {
-  //     __REVOLUT_USERNAME__: JSON.stringify("razsbg"),
-  //   },
-  // },
-  // site:
-  //   process.env.NODE_ENV === "production"
-  //     ? "https://party.razsbg.top"
-  //     : "http://localhost:4000",
+  adapter: node({ mode: "standalone" }),
+  site:
+    process.env.NODE_ENV === "production"
+      ? "https://party.razsbg.top"
+      : "http://localhost:4000",
+  output: "server", // SSR mode for Railway
+  server: {
+    port: parseInt(process.env.PORT || "4321"),
+    host: "0.0.0.0",
+  },
 })
